f2365bb2eed0aad41295a501154f5ee108fd1a8e
Brendah Edward <edwardbrendah28@gmail.com>
2025-05-16 11:49:08 +0300

    relationship methods

--- coffee.py
+++ coffee.py
@@ -19,3 +19,13 @@ class Coffee:
 
     def customers(self):
         return list(set(order.customer for order in self.orders()))
+
+    def num_orders(self):
+        return len(self.orders())
+
+    def average_price(self):
+        orders = self.orders()
+        if not orders:
+            return 0
+        total = sum(order.price for order in orders)
+        return total / len(orders)
